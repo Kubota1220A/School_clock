4c4b8e5eb0a94194f12b5708efd4a22db481f73b
ASA <s24056@tokyo.kosen-ac.jp>
2025-05-02 08:54:03 +0900

    fix index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function startClocks() {
     
     // 各時計の設定
     const clocks = [
-        { id: 'clock1', offset: 0.5 },
+        { id: 'clock1', offset: 500 },
         { id: 'clock2', offset: -2000 }, // 自学自習室の時計を2秒遅らせる
         { id: 'clock3', offset: 0 }, // 2分進める
         { id: 'clock4', offset: 57 * 1000 }, // 電気電子工学実験室を57秒進める
